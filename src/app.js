4b9a43b70aaad7867cdb69f24d6223f8006920ec
Erin Gurley <erinmgurley@gmail.com>
2019-04-24 15:02:20 -0700

    Display and track wins and losses.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@ const submitButton = document.getElementById('submit');
 const donkeySide = document.getElementById('donkey');
 const messageValue = document.getElementById('message');
 const headsGuess = document.getElementById('heads');
+const winsCount = document.getElementById('wins');
+const lossesCount = document.getElementById('losses');
+
+// defining scoring variables
+let wins = 0;
+let losses = 0;
 
 submitButton.addEventListener('click', () => {
     const randomNum = Math.random();
@@ -20,10 +26,14 @@ submitButton.addEventListener('click', () => {
     }
 
     if(flip === userGuess) {
-        messageValue.textContent = 'You Won!'; 
+        messageValue.textContent = 'You Won!';
+        wins++;
+        winsCount.textContent = 'Wins: ' + wins; 
     }
     else {
         messageValue.textContent = 'You Lose!';
+        losses++;
+        lossesCount.textContent = 'Losses: ' + losses; 
     }
    
 });
