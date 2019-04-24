723be75eade4671977a6c125c2e4332fba075f33
Erin Gurley <erinmgurley@gmail.com>
2019-04-24 14:11:55 -0700

    moved coinToss function into it's own file

--- test/tests.js
+++ test/tests.js
@@ -1,16 +1,25 @@
+import coinToss from '../src/coin-toss.js';
 const test = QUnit.test;
-
-function coinToss(number) {
-    if(number < .5){
-        return 'heads';
-    }
-}
 
 test('test for <.5 then return heads', function(assert) {
     //Arrange
     // Set up your parameters and expectations
     const number = .49;
     const expected = 'heads';
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = coinToss(number);
+
+    //Assert
+    assert.equal(result, expected);
+});
+
+test('test for >=.5 then return tails', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const number = .5;
+    const expected = 'tails';
 
     //Act 
     // Call the function you're testing and set the result to a const
