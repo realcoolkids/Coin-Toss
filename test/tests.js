f55fb99490b9a65a5178fb6753f7d14bada6430c
Erin Gurley <erinmgurley@gmail.com>
2019-04-24 13:58:04 -0700

    Added a test to give the function less than .5 and return heads.

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,21 @@
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+function coinToss(number) {
+    if(number < .5){
+        return 'heads';
+    }
+}
+
+test('test for <.5 then return heads', function(assert) {
     //Arrange
     // Set up your parameters and expectations
+    const number = .49;
+    const expected = 'heads';
 
     //Act 
     // Call the function you're testing and set the result to a const
+    const result = coinToss(number);
 
     //Assert
-    assert.equal(true, false);
+    assert.equal(result, expected);
 });
