85d72b43d88c7942ea385ed64de380256dcf402b
Erin Gurley <erinmgurley@gmail.com>
2019-04-24 14:54:15 -0700

    connected radio buttons and displayed win/lose based on selection

--- src/app.js
+++ src/app.js
@@ -1,10 +1,30 @@
 import coinToss from './coin-toss.js';
 const submitButton = document.getElementById('submit');
 const donkeySide = document.getElementById('donkey');
+const messageValue = document.getElementById('message');
+const headsGuess = document.getElementById('heads');
 
 submitButton.addEventListener('click', () => {
     const randomNum = Math.random();
     const flip = coinToss(randomNum);
     const image = 'assets/' + flip + '.jpg';
     donkeySide.src = image;
+
+    let userGuess = '';
+
+    if(headsGuess.checked) {
+        userGuess = 'heads';
+    }
+    else { 
+        userGuess = 'tails';
+    }
+
+    if(flip === userGuess) {
+        messageValue.textContent = 'You Won!'; 
+    }
+    else {
+        messageValue.textContent = 'You Lose!';
+    }
+   
 });
+
