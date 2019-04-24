d3ffb10360b64125b3f1ea0219ce68f4044b253f
Erin Gurley <erinmgurley@gmail.com>
2019-04-24 14:26:27 -0700

    Make image correspond with flip value.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
+import coinToss from './coin-toss.js';
 const submitButton = document.getElementById('submit');
+const donkeySide = document.getElementById('donkey');
 
 submitButton.addEventListener('click', () => {
-    console.log('submit');
+    const randomNum = Math.random();
+    const flip = coinToss(randomNum);
+    const image = 'assets/' + flip + '.jpg';
+    donkeySide.src = image;
+
+    console.log(donkeySide.src);
 });
