ad3fd6d21de745ab0d4460ae4711efd2321567e9
Erin Gurley <erinmgurley@gmail.com>
2019-04-24 16:04:50 -0700

    Connected the money counter with bet input

--- src/app.js
+++ src/app.js
@@ -5,10 +5,13 @@ const messageValue = document.getElementById('message');
 const headsGuess = document.getElementById('heads');
 const winsCount = document.getElementById('wins');
 const lossesCount = document.getElementById('losses');
+const betInput = document.getElementById('bet-amount');
+const moneyValue = document.getElementById('money');
 
 // defining scoring variables
 let wins = 0;
 let losses = 0;
+let money = 1000;
 
 submitButton.addEventListener('click', () => {
     const randomNum = Math.random();
@@ -25,12 +28,16 @@ submitButton.addEventListener('click', () => {
         userGuess = 'tails';
     }
 
+    const betValue = parseInt(betInput.value, 10);
+
     if(flip === userGuess) {
         messageValue.textContent = 'You Won!';
         wins++;
         winsCount.textContent = 'Wins: ' + wins; 
         messageValue.classList.remove('loss');
         messageValue.classList.add('win');
+
+        money += betValue;
     }
     else {
         messageValue.textContent = 'You Lose!';
@@ -38,7 +45,11 @@ submitButton.addEventListener('click', () => {
         lossesCount.textContent = 'Losses: ' + losses; 
         messageValue.classList.remove('win');
         messageValue.classList.add('loss');
+
+        money -= betValue;
     }
    
+    moneyValue.textContent = 'Money: $' + money;
+
 });
 
